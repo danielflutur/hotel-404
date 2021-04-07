7b80f4a7c060db3e208f3dfd04661af680f4794f
Flutur Daniel Ioan <ioan.flutur@student.usv.ro>
2021-04-07 18:54:30 +0300

    Updated database

--- connection.php
+++ connection.php
@@ -1,8 +1,12 @@
-<?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "hotel_db";  
+<?php 
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));     
+    $host = $cleardb_url["host"];  
+    $user = $cleardb_url["user"];  
+    $password = $cleardb_url["pass"];  
+    $db_name = substr($cleardb_url["path"],1);  
+    
+    $active_group = 'default';
+	$query_builder = TRUE;
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
