ba711921591838b8148cb15f2486a43e2314335a
Flutur Daniel Ioan <ioan.flutur@student.usv.ro>
2021-04-09 00:43:55 +0300

    update

--- orders/myOrders.php
+++ orders/myOrders.php
@@ -90,6 +90,7 @@
       $data = mysqli_query($con,$query);
       while($result=mysqli_fetch_assoc($data))
       { 
+        sort($result);
         echo "<div class='py-1' style='transform: translateY(10%);'>
           <div class='container'>
             <div class='row'>";
